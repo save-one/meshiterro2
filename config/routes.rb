306693a40206655c06a874fbd30829d62ded896a
takahiro <nobooknolife16@gmail.com>
2018-03-15 17:46:20 +0900

    finish 5-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+
   devise_for :users
 
-  resources :post_images, only: [:new, :create, :index, :show] do
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
   	resource :favorites, only: [:create, :destroy]
   	resource :post_comments, only: [:create, :destroy]
   end
+
+  resources :users, only: [:show]
 
 
   root 'post_images#index'
